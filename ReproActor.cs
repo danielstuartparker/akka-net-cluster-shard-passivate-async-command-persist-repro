3fa1e5f2b7b0477dd146328f571a975e907acbbf
Gregorius Soedharmo <arkatufus@yahoo.com>
2022-10-08 01:03:31 +0700

    Defer actor stop until all async operations are complete

--- ReproActor.cs
+++ ReproActor.cs
@@ -8,13 +8,9 @@ namespace AsyncCommandBug
 {
     public class ReproActor : ReceivePersistentActor
     {
-        public override string PersistenceId
-        {
-            get
-            {
-                return Self.Path.Name;
-            }
-        }
+        private int _asyncOperations = 0;
+        private bool _stopping = false;
+        public override string PersistenceId => Self.Path.Name;
 
         public ReproActor()
         {
@@ -27,18 +23,28 @@ namespace AsyncCommandBug
             Command<ReceiveTimeout>(receiveTimeout =>
             {
                 Console.WriteLine("Passivating Shard");
-                Context.Parent.Tell(new Passivate(PoisonPill.Instance));
-                this.SetReceiveTimeout(null);
+                Context.Parent.Tell(new Passivate(new StopEntity()));
+                SetReceiveTimeout(null);
+            });
+
+            Command<StopEntity>(_ =>
+            {
+                _stopping = true;
+                if(_asyncOperations == 0)
+                    Context.Stop(Self);
             });
 
             CommandAsync<LongAsyncMessage>(async msg => {
+                _asyncOperations++;
                 Console.WriteLine("Async Command Received");
                 await Task.Delay(2000); // simulate doing work, like an external REST call
                 Console.WriteLine("Async Command Processed");
-                Persist("result", result => {
+                Persist("result", result =>
+                {
                     Console.WriteLine("Async Command Results Persisted");
                     shardRegion.Tell(new NonAsyncMessage());
                     shardRegion.Tell(new AsyncMessage());
+                    _asyncOperations--;
                 });
             });
 
@@ -50,8 +56,10 @@ namespace AsyncCommandBug
                 });
             });
 
-            CommandAsync<AsyncMessage>(async msg =>
+            CommandAsync<AsyncMessage>(msg =>
             {
+                _asyncOperations++;
+                var self = Self;
                 Console.WriteLine("AsyncMessage Received");
                 Persist("result", result => {
                     // BUG
@@ -60,8 +68,15 @@ namespace AsyncCommandBug
                     // and this will never run
                     // https://getakka.net/api/Akka.Persistence.ReceivePersistentActor.html#Akka_Persistence_ReceivePersistentActor_CommandAsync_System_Type_System_Predicate_System_Object__System_Func_System_Object_System_Threading_Tasks_Task__
                     Console.WriteLine("AsyncMessage Persisted");
+                    
+                    // This test should only be done at the leaf operations
+                    _asyncOperations--;
+                    if (_stopping && _asyncOperations == 0)
+                    {
+                        Context.Stop(self);
+                    }
                 });
-                await Task.Yield();
+                return Task.CompletedTask;
             });
         }
 
